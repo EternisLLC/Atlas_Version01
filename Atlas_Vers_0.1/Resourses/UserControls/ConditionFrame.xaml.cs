28322ce0d500800eb1679de14cc438040bc8886e
Эдгар Саркисян <edgar.sarkisyan2002@mail.ru>
2022-03-02 16:54:20 +0300

    /

--- Atlas_Vers_0.1/Resourses/UserControls/ConditionFrame.xaml.cs
+++ Atlas_Vers_0.1/Resourses/UserControls/ConditionFrame.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Atlas_Vers_0._1.ViewModels;
+using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -9,10 +11,33 @@ namespace Atlas_Vers_0._1.Resourses.UserControls
     /// </summary>
     public partial class ConditionFrame : UserControl
     {
+        public ObservableCollection<Unit> Units { get; set; }
+
         public ConditionFrame()
         {
             InitializeComponent();
+
+            Units = new ObservableCollection<Unit>
+            {
+                new Unit {Id = 1, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 15, secondTemp = 30},
+                new Unit {Id = 2, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 17, secondTemp = 40},
+                new Unit {Id = 3, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 18, secondTemp = 50},
+                new Unit {Id = 4, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 19, secondTemp = 60},
+                new Unit {Id = 5, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 30, secondTemp = 70},
+                new Unit {Id = 6, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 30, secondTemp = 70},
+                new Unit {Id = 7, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 30, secondTemp = 70},
+                new Unit {Id = 8, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 30, secondTemp = 70},
+                new Unit {Id = 9, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 30, secondTemp = 70}
+            };
+            FireSituationList.ItemsSource = Units;
+
             btnFireSituation.Focus();
+        }
+
+        private void phonesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Unit p = (Unit)FireSituationList.SelectedItem;
+            MessageBox.Show(p.Image);
         }
 
         private void btnFireSituation_GotFocus(object sender, RoutedEventArgs e)
--- Atlas_Vers_0.1/ViewModels/BurViewModel.cs
+++ Atlas_Vers_0.1/ViewModels/BurViewModel.cs
@@ -14,29 +14,12 @@ namespace Atlas_Vers_0._1.ViewModels
         public string Image { get; set; }
         public int firstTemp { get; set; }
         public int secondTemp { get; set; }
-
-        
     }
 
 
 
     public class BurViewModel : ViewModel
     {
-        public ObservableCollection<Unit> Units { get; set; }
-
-        public void AddUnits()
-        {
-            Units = new ObservableCollection<Unit>
-            {
-                new Unit {Id = 1, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 15, secondTemp = 30},
-                new Unit {Id = 2, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 17, secondTemp = 40},
-                new Unit {Id = 3, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 18, secondTemp = 50},
-                new Unit {Id = 4, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 19, secondTemp = 60},
-                new Unit {Id = 5, Image="/Resourses/Pictures/BUR/BUR_Break_Transparent@4x.png", firstTemp = 30, secondTemp = 70}
-            };
-
-        }
         
-
     }
 }
